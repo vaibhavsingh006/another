1f758dd40c307f78dd4a45c748851e44a76b4f68
pawnesh <pawneshkitio@gmail.com>
2026-05-16 15:31:30 +0530

    code push

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const protect = require('../middlewares/authMiddleware');
 const { signup, loginUser, getMe, logout } = require('../controllers/authController');
 
-router.post('/register', signup);
 router.post('/signup', signup);
 router.post('/login', loginUser);
 router.post('/logout', logout);
