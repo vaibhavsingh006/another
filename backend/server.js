a2f7c01beaaba7710ca9150b0ab1b2530a3677dd
pawnesh <pawneshkitio@gmail.com>
2026-05-16 16:36:01 +0530

    kd

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,24 @@ const cartRoutes = require('./routes/cartRoutes');
 env.config();
 
 const app = express();
-app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'https://famous-daifuku-c1a435.netlify.app',
-    ],
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://famous-daifuku-c1a435.netlify.app',
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
